f00a7a0127195f23d4b580196914efebc56777f1
UNeB - Murilo Santos <murilo.santos@adventistas.org>
2023-01-17 14:00:23 -0300

    primeiro

---
+++ src/dominio/Dev.java
@@ -0,0 +1,44 @@
+package dominio;
+
+import java.util.Set;
+
+public class Dev {
+	
+	private String nome;
+	private Set<Curso> mentorias;
+	private Set<Curso> atividades;
+	
+	public Dev() {}
+
+	public Dev(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Set<Curso> getMentorias() {
+		return mentorias;
+	}
+
+	public void setMentorias(Set<Curso> mentorias) {
+		this.mentorias = mentorias;
+	}
+
+	public Set<Curso> getAtividades() {
+		return atividades;
+	}
+
+	public void setAtividades(Set<Curso> atividades) {
+		this.atividades = atividades;
+	}
+	
+	
+	
+
+}
